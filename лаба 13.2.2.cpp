2d0785e1d272adb7c701cf24727dc2fbfb4942ee
Khrystyna <hrustunakopeika2005@gmail.com>
2022-12-22 18:11:17 +0200

    first master commit

---
+++ лаба 13.2.2.cpp
@@ -0,0 +1,60 @@
+﻿// лаба 13.2.2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+#include <iostream>
+
+using namespace std;
+
+void Audit(double sum_rad);
+void Suma(double radian_A, double radian_B, double radian_C, double& sum_radian);
+
+#define SUM(x,y,z) (x)+(y)+(z)
+#define RAD(gradian, PI) (gradian)*((PI) / 180)
+#define GRAD(radian, PI) (radian) * (180 / (PI))
+
+void main()
+{
+	puts("Lab 13.2. Using macros and preprocessing directive");
+	const double PI = 4 * atan(1);
+	double gradian_A;
+	double gradian_B;
+	double gradian_C;
+	cout << "Gradus A = "; cin >> gradian_A;
+	cout << "Gradus B = "; cin >> gradian_B;
+	cout << "Gradus C = "; cin >> gradian_C;
+	double radian_A;
+	double radian_B;
+	double radian_C;
+	double sum_gradian;
+	double sum_radian;
+	sum_gradian = SUM(gradian_A, gradian_B, gradian_C);
+	if (sum_gradian != 180)
+	{
+		cout << sum_gradian << endl;
+		cout << "This triangle is not correct" << endl;
+	}
+	else
+	{
+		radian_A = RAD(gradian_A, PI);
+		radian_B = RAD(gradian_B, PI);
+		radian_C = RAD(gradian_C, PI);
+		cout << "Radian A = " << radian_A << endl;
+		cout << "Radian B = " << radian_B << endl;
+		cout << "Radian C = " << radian_C << endl;
+		Suma(radian_A, radian_B, radian_C, sum_radian);
+		cout << "Sum of all cos of radian = " << sum_radian << endl;
+		Audit(sum_radian);
+	}
+}
+void Suma(double radian_A, double radian_B, double radian_C, double& sum_radian) {
+	sum_radian = SUM(cos(radian_A), cos(radian_B), cos(radian_C));
+}
+void Audit(double sum_radian)
+{
+	if (sum_radian <= 3. / 2)
+		cout << "True" << endl;
+	else
+		cout << "False" << endl;
+}
---
+++ юніт тест 13.2.2.cpp
@@ -0,0 +1,27 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../лаба 13.2.2/лаба 13.2.2.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace юніттест1322
+{
+	TEST_CLASS(юніттест1322)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			const double PI = 4 * atan(1);
+			double grad_A = 60;
+			double grad_B = 60;
+			double grad_C = 60;
+			double rad_A = grad_A * (PI / 180);
+			double rad_B = grad_B * (PI / 180);
+			double rad_C = grad_C * (PI / 180);
+			double sum_rad;
+			Suma(rad_A, rad_B, rad_C, sum_rad);
+			Assert::AreEqual(sum_rad, 1, 5);
+		}
+	};
+}
